be0d42297ba516551ed60538fdad548c99020b14
panuwat <panuwat.codes@gmail.com>
2025-05-26 16:34:41 +0700

    refactor: not used

--- foo.js
+++
@@ -1,9 +0,0 @@
-const foo = "baz";
-
-const email = 'test@example.com';
-console.log(email); // ❌
-
-const user = { email: 'test@example.com', age: 30 };
-console.log(user); // ❌
-
-console.info({ phone: '1234567890' }); // ❌
